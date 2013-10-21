fe2d9a07761192dd72bf66f2388ad3aa7eb2277a
Nick Brown <nick@nickbrown.me>
2013-10-21 16:07:41 -0700

    Commented the retrieve method

--- app/controllers/ImportController.php
+++ app/controllers/ImportController.php
@@ -11,13 +11,19 @@ class ImportController extends BaseController {
 
 	public function retrieve()
 	{
+        // Get user-defined start date
         $start_date = Input::get('start_date');
-        $end_date = date('Y-m-d', strtotime('+1 week', strtotime($start_date)));
-        
+
+        // Set end date a week from the start date
+        // $end_date = date('Y-m-d', strtotime('+1 week', strtotime($start_date)));
+        $end_date = date('Y-m-d'); 
+
+        // Define GA account constants
         define('ga_email', '***REMOVED***');
         define('ga_password', '***REMOVED***');
         define('ga_profile_id', '***REMOVED***');
 
+        // Create a new Request object and retrieve our first 7 dimensions
         $completions = new GoogleRequest(ga_email, ga_password);
 
         $completions->requestReportData(
@@ -30,8 +36,10 @@ class ImportController extends BaseController {
             $end_date
         );
 
+        // Save our dimensions
         $results = $completions->getResults();
 
+        // Request a new set of data so we can add hostname
         $completions->requestReportData(
             ga_profile_id,
             array('hostname', 'customVarValue1', 'customVarValue2', 'country', 'region', 'city', 'latitude'),
@@ -42,24 +50,27 @@ class ImportController extends BaseController {
             $end_date 
         );
 
+        // Iterate over completions, combining the data into one array
         foreach($completions->getResults() as $k => $entry)
         {
-            $entry->pushToDimensions(['customVarValue2' => $results[$k]->getDimensions()['customVarValue2']]);
+            $entry->pushToDimensions(['longitude' => $results[$k]->getDimensions()['longitude']]);
 
             $metrics = $entry->getMetrics();
             $dimensions = $entry->getDimensions();
 
-            $res[] = array_merge($metrics, $dimensions);
+            $combined[] = array_merge($metrics, $dimensions);
         }
 
-        if( 1 == 1 )
+        // If this is a test run, pass data to the test method and return the resulting view
+        if( Input::get('test') == 1 )
         {
-            return $this->test($results, $completions->getResults(), $res);
+            return $this->test($results, $completions->getResults(), $combined);
         }
         
-        foreach($completions->getResults() as $k => $entry)
+        // If we're still running at this point, instatiate and save our completions
+        foreach($combined as $completion)
         {
-            $compl = new Completion($res);
+            $compl = new Completion($completion);
             $compl->start_of_week = $start_date;
 			$compl->save();
         }
@@ -71,50 +82,4 @@ class ImportController extends BaseController {
     {
         return View::make('import.test', ['first' => $first, 'second' => $second, 'combined' => $combined]);
     }
-
-	//public function test()
-	//{
-    //    $start_date = '2013-09-01';
-    //    $end_date = '2013-10-15';
-    //    
-    //    define('ga_email', '***REMOVED***');
-    //    define('ga_password', '***REMOVED***');
-    //    define('ga_profile_id', '***REMOVED***');
-
-    //    $completions = new GoogleRequest(ga_email, ga_password);
-
-    //    $completions->requestReportData(
-    //        ga_profile_id,
-    //        array('customVarValue1', 'customVarValue2', 'country', 'region', 'city', 'latitude', 'longitude'),
-    //        array('pageviews', 'uniquePageviews', 'goal16Completions', 'goal14Completions'),
-    //        array('region'),
-    //        'pagePath=~material-profile',
-    //        $start_date,
-    //        $end_date
-    //    );
-
-    //    $results = $completions->getResults();
-
-    //    $completions->requestReportData(
-    //        ga_profile_id,
-    //        array('hostname', 'customVarValue1', 'customVarValue2', 'country', 'region', 'city', 'latitude'),
-    //        array('pageviews', 'uniquePageviews', 'goal16Completions', 'goal14Completions'),
-    //        array('region'),
-    //        'pagePath=~material-profile',
-    //        $start_date,
-    //        $end_date 
-    //    );
-
-    //    foreach($completions->getResults() as $k => $entry)
-    //    {
-    //        $entry->pushToDimensions(['longitude' => $results[$k]->getDimensions()['longitude']]);
-
-    //        $metrics = $entry->getMetrics();
-    //        $dimensions = $entry->getDimensions();
-
-    //        $res[] = array_merge($metrics, $dimensions);
-    //    }
-    //    
-    //    return View::make('import.test', ['first' => $results, 'second' => $completions->getResults(), 'combined' => $res]);
-    //}
 }
