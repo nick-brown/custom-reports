3db04e02c4333f4e07959ff50c48ff7867071730
Nick Brown <nick@nickbrown.me>
2013-10-21 16:07:16 -0700

    Added test? checkbox

--- app/views/import/index.blade.php
+++ app/views/import/index.blade.php
@@ -5,6 +5,9 @@
         {{ Form::label('start_date', 'Start Date') }} 
 		{{ Form::text('start_date', $last_sunday) }}
         <br>
+        {{ Form::label('test', 'Test Run?') }}
+        {{ Form::checkbox('test', '1', true) }}
+        <br>
         {{ Form::submit('Import') }}
     {{ Form::close() }}
 @stop
