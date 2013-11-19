735629bd2681a928147ea66a22180d32f3dfb312
Nick Brown <nick@nickbrown.me>
2013-11-19 09:24:49 -0700

    Added dynamic base paths to JavaScript

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -7,8 +7,12 @@
 	
 	<body>
 		@yield('body')
-        
         <!-- Scripts -->
+        <script>
+            window.config = {
+                public: '<% addslashes(public_path()) %>\\'
+            };
+        </script>
         <% HTML::script('//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js') %>
         @yield('scripts')
         <!-- Scripts End -->
