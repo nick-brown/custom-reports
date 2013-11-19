741110b764eda58a274ccec1d97608004b07ffd0
Nick Brown <nick@nickbrown.me>
2013-11-19 09:25:06 -0700

    Updated dropdowns to angular-friendly versions

--- app/views/report/index.blade.php
+++ app/views/report/index.blade.php
@@ -2,41 +2,47 @@
 
 @section('scripts')
     <% HTML::script('//ajax.googleapis.com/ajax/libs/angularjs/1.0.7/angular.min.js') %>
+    <% HTML::script('//ajax.googleapis.com/ajax/libs/angularjs/1.0.7/angular-resource.min.js') %>
     <% HTML::script('js/app.js') %>
-    <script>
-        var analytics = { 
-            completions: <% $completions %>,
-            events: <% $events %>
-        };
-    </script>
 @stop
 
 @section('body')
-    <div id="filters">
-       Filter by: 
-        <% Form::select('start_of_week', $sundays) %> &amp;
-        <% Form::select('channelPartner', $channel_partners) %> &amp;
-        <% Form::select('materialName', $materials) %>
-    </div>
+    <div ng-app="app">
+        <div id="filters" ng-controller="DropdownCtrl">
+           Filter by:
+            <select ng-options="sundays in dates">
+                <option value="">Choose week...</option>
+            </select>
+            &amp;
+            <select ng-options="partner in channelPartners">
+                <option value="">Choose week...</option>
+            </select>
+            &amp;
+            <select ng-options="material in materials">
+                <option value="">Choose week...</option>
+            </select>
+            &amp;
+        </div>
 
-    <h2>DLMPT Profile Pages Report for the week of $week</h2>
-    <h3>For $channelPartner profile pages</h3>
+        <h2>DLMPT Profile Pages Report for the week of $week</h2>
+        <h3>For $channelPartner profile pages</h3>
 
-    <br>
+        <br>
 
-    <h3>Totals</h3>
+        <h3>Totals</h3>
 
-    <div id="totals">
-        DLMPT Leads: 
-        <br>
-        All Leads including DLMPT Leads:
-        <br>
-        Image Clicks:
-        <br>
-        Button Clicks:
-        <br>
-        Unique Visitors:
-        <br>
-        Pageviews:
+        <div id="totals">
+            DLMPT Leads:
+            <br>
+            All Leads including DLMPT Leads:
+            <br>
+            Image Clicks:
+            <br>
+            Button Clicks:
+            <br>
+            Unique Visitors:
+            <br>
+            Pageviews:
+        </div>
     </div>
 @stop
