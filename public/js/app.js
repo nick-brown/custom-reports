1a98b5fd0e075d619a8bb9e29e2f4adde6962b3d
Nick Brown <nick@nickbrown.me>
2013-11-20 14:35:52 -0700

    added filter to select correct startOfWeek

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 angular.module('app', ['ngResource'])
-    .controller('DropdownCtrl', ['$scope', '$http', function($scope, $http) {
+    .controller('DropdownCtrl', ['$scope', '$http', '$filter', function($scope, $http, $filter) {
         // Consider using $resource instead of $http
         $http({
             method: 'GET',
@@ -9,11 +9,39 @@ angular.module('app', ['ngResource'])
             $scope.materials = data.materials;
             $scope.channelPartners = data.partners;
             $scope.sundays = data.sundays;
+
+            // Add completions for total DLMPT Leads
+            var completions = [];
+            $.each(data.analytics.completions, function(k, v) {
+                completions.push(v.goal16Completions);
+                completions.push(v.goal14Completions);
+            });
+
+            var sum = completions.reduce(function(previous, current) {
+                return previous + current;
+            });
+
+            var selectedDates = $filter('week')(data.analytics.completions, '2013-11-03');
+            console.log(selectedDates);
+
         })
         .error(function(data, status, headers, config) {
             console.log('no data could be retrieved: ' + status);
         });
     }])
+    .filter('week', function() {
+        return function (input, startOfWeek) {
+            var out = [];
+
+            for(var x = 0; x < input.length; x++) {
+                if(input[x].start_of_week === startOfWeek) {
+                    out.push(input[x]);
+                }
+            }
+
+            return out;
+        }
+    });
 //    .service('sharedProperties', function () {
 //        var partner = '';
 //
